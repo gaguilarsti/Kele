2feaf22b609c3ca3bb6cd43631212ee2f680a0be
Gama Aguilar <gama@9104studios.com>
2017-01-14 16:28:59 -0800

    Checkpoint 6 done for reals this time

--- lib/kele/messages.rb
+++ lib/kele/messages.rb
@@ -11,8 +11,18 @@ module Messages
     else
       response = self.class.get(api_endpoint("message_threads?page=#{page}"), headers: { "authorization" => @auth_token })
       @messages = JSON.parse(response.body)
-
     end
+  end
+
+  def create_message(sender_email, recipient_id, subject, message)
+    response = self.class.post(api_endpoint("messages"), body: {
+      "sender": sender_email,
+      "recipient_id": recipient_id,
+      "subject": subject,
+      "stripped-text": message }, headers: { "authorization" => @auth_token })
+
+      puts response 
+
   end
 
 
