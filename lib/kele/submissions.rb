1195057ebd4fbd9b87723ca934785a275ebaba8e
Gama Aguilar <gama@9104studios.com>
2017-01-14 19:11:28 -0800

    checkpoint 7 completed

--- lib/kele/submissions.rb
+++ lib/kele/submissions.rb
@@ -6,6 +6,8 @@ module Submissions
 
   def create_submission(checkpoint_id, enrollment_id, assign_branch: nil, assign_comm_link: nil, comment: nil)
 
+    # adding comment 
+
     response = self.class.post(api_endpoint("checkpoint_submissions"), body: {
       "assignment_branch": assign_branch,
       "assignment_commit_link": assign_comm_link,
